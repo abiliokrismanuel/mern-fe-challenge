741f9a6dc14d0d03f887d3f3c2609917882275c3
Abilio Krismanuel <abilio.krismanuel@ti.ukdw.ac.id>
2022-11-26 00:26:30 +0700

    add axios and material ui

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
+import React, { useEffect, useState} from 'react';
+import axios from 'axios';
+
+import { Typography } from '@mui/material';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [location, setLocation] = useState("loading");
+
+  useEffect(() =>{
+    const getLocation = async () => {
+      let result = await axios.get("http://localhost:3001/location")
+      setLocation(result.data.location)
+    }
+
+    getLocation()
+  },[])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Typography variant={"h4"}>{location}</Typography>
       </header>
     </div>
   );
